f9323963a1c7eb35a156f805d2ba7c1863b3431e
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-03-04 17:39:54 +0530

    Attempted to make a C system. Need to study more

---
+++ 0-Blinky/C/main.c
@@ -0,0 +1,26 @@
+#define RCC_BASE 0x40021000
+#define RCC_APB2ENR *((volatile unsigned int *)(RCC_BASE + 0x18))
+
+#define GPIOC_BASE 0x40011000
+#define GPIOC_CRH *((volatile unsigned int *)(GPIOC_BASE + 0x04))
+#define GPIOC_BSRR *((volatile unsigned int *)(GPIOC_BASE + 0x10))
+
+void delay(unsigned int da)
+{
+	while(da--)
+		asm("nop");
+}
+
+void main()
+{
+	RCC_APB2ENR = 1 << 4;
+	GPIOC_CRH |= 0b11 << 21;
+	GPIOC_CRH &= ~(0b00) < 23;
+	while(1)
+	{
+		GPIOC_BSRR |= (1 << 29);
+		delay(1000000);
+		GPIOC_BSRR |= (1 << 13);
+		delay(1000000);
+	}
+}
