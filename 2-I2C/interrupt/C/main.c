0b15ee99934ddb31caae0d61a07cd0395ff67fee
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-09-09 22:56:21 +0530

    I2C working without interrupts in ASM. Fucken datasheet doesn't have enough details

--- 2-I2C/interrupt/C/main.c
+++ 2-I2C/interrupt/C/main.c
@@ -4,22 +4,12 @@
 #define GPIOC_BASE 0x40011000
 #define GPIOC_CRH *((volatile unsigned int *)(GPIOC_BASE + 0x04))
 #define GPIOC_BSRR *((volatile unsigned int *)(GPIOC_BASE + 0x10))
-
-void delay(unsigned int da)
-{
-	while(da--);
-}
-
 void main()
 {
 	RCC_APB2ENR = 1 << 4;
 	GPIOC_CRH |= 0b11 << 21;
-	GPIOC_CRH &= ~(0b00) < 23;
+	GPIOC_BSRR |= (1 << 29);
 	while(1)
-	{	
-		GPIOC_BSRR |= (1 << 29);
-		delay(8000000);
-		GPIOC_BSRR |= (1 << 13);
-		delay(8000000);
+	{
 	}
 }
